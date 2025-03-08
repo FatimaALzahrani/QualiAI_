b484f9cf7498c286b9c347e77821a03b9c85ad6d
Fatimah Alzahrani <12fatimah.15@gmail.com>
2025-03-08 19:41:19 +0300

    .

--- app.py
+++ app.py
@@ -1545,5 +1545,6 @@ def view_report(report_id):
     else:
         abort(404, description="التقرير غير موجود")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
