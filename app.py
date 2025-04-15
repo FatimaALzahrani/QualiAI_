62c2c91b7194224536beaaa6db4445f534d19fbe
Fatimah Alzahrani <12fatimah.15@gmail.com>
2025-04-15 13:52:11 +0300

    Finally :)

--- app.py
+++ app.py
@@ -1670,5 +1670,4 @@ def update_report(report_id):
     return redirect(url_for('view_final_report', report_id=report_id))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=10000)
